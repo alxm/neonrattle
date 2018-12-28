723882fb9fd0ab6faea4ee2c02f5493083ea0eee
Alex Margarit <alex@alxm.org>
2018-12-28 10:02:49 -0800

    Add Emscripten makefile.

---
+++ make/makeall.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+output=""
+make_arg=$1
+
+color_text ()
+{
+    output="$output\n\033[$1;1m$2\033[0m"
+}
+
+record_result ()
+{
+    if [ $2 -eq 0 ];
+    then
+        color_text 32 "[ Success ] $1"
+    else
+        color_text 31 "[ Failure ] $1"
+    fi
+}
+
+do_make ()
+{
+    make -j -f $2 $make_arg
+    record_result "$1" $?
+}
+
+do_emmake ()
+{
+    emmake make -j -f $2 $make_arg
+    record_result "$1" $?
+}
+
+source /opt/emsdk-portable/emsdk_env.sh
+
+do_make "Dev" Makefile
+do_emmake "Emscripten" Makefile.emscripten
+
+echo -e $output
