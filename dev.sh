7d84fd7674005ff5b57b5fc1fbd537abf123f6fc
Alex Margarit <alex@alxm.org>
2019-01-21 10:49:13 -0800

    Misc updates.

--- dev.sh
+++ dev.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 geany -i neonrattle/* make/* assets/gfx/*.py assets/sfx/*.py dev.sh README.md &
 arduino neonrattle/neonrattle.ino &
-gimp -n assets/gfx/*.png assets/gfx/*.BMP &
+gimp -n assets/gfx/palette.png assets/gfx/maps_grid16x16.xcf &
 cd make
